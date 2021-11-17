bde8473881956073fb66107d3826bcde9aee2da2
itsmegr <rathor_941919@student.nitw.ac.in>
2021-11-17 23:40:46 +0530

    changed to newer vesrsion 1.0.1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 
-console.log("welcome to the world, you have successfully created npm package")
+console.log("welcome to the world, you have successfully published npm package")
 
